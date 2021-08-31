0520bd80c06d201bab854037bc40b377a5f9a98c
andresgoag <andresgomezagudelo@hotmail.com>
2021-08-31 00:45:57 +0000

    Domain name generator ver 1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let fours = [".com", ".net", ".us"];
+
+  const domain_container = document.querySelector("#domain-container ul");
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        for (let four of fours) {
+          domain_container.insertAdjacentHTML(
+            "beforeend",
+            `<li>${pronoun + adj + noun + four}</li>`
+          );
+        }
+      }
+    }
+  }
 };
